bbf6518b18801950214f87de1498ce1de843a821
Higor braga <higorbraga00@gmail.com>
2021-11-11 15:09:19 -0300

    Implementando geracao de token com JWT

---
+++ src/main/java/br/com/alura/forum/dto/form/LoginForm.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.dto.form;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class LoginForm {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public UsernamePasswordAuthenticationToken converter() {
+        return  new UsernamePasswordAuthenticationToken(email, senha);
+    }
+}
---
+++ src/main/java/br/com/alura/forum/service/TokenService.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.service;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private Long expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authenticate) {
+        Usuario logado = (Usuario) authenticate.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + expiration);
+        return Jwts.builder()
+                .setIssuer("API Fórum da Alura")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact();
+    }
+}
